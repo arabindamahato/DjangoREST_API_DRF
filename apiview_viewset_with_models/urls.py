5d91bf4890c711ad68cbbe45aaf0efe42da53453
Arabinda Mahato <abm21719@gmail.com>
2020-04-14 21:29:16 +0530

    List Create Retrieve Update Destroy ListCreate RetrieveUpdate RetrieveDestroy RetrieveUpdateDestroy ALL ARE DONE.

--- apiview_viewset_with_models/urls.py
+++ apiview_viewset_with_models/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from apiview_viewset_with_models import views
 
 urlpatterns = [
-	path('apiview-with-models-list/', views.EmployeeListAPIView.as_view()),
-	path('apiview-with-models-create/', views.EmployeeCreateAPIView.as_view()),
+	# path('apiview-list/', views.EmployeeListAPIView.as_view()),
+	# path('apiview-create/', views.EmployeeCreateAPIView.as_view()),
+	# path('apiview-retrieve/<pk>/', views.EmployeeRetrieveAPIView.as_view()),
+	# path('apiview-update/<pk>/', views.EmployeeUpdateAPIView.as_view()),
+	path('apiview-listcreate/', views.EmployeeListCreateAPIView.as_view()),
+	# path('apiview-retrieveupdate/<pk>', views.EmployeeRetrieveUpdateAPIView.as_view()),
+	# path('apiview-retrievedestroy/<pk>', views.EmployeeRetrieveDestroyAPIView.as_view()),
+	path('apiview-retrieveupdatedestroy/<pk>', views.EmployeeRetrieveUpdateDestroyAPIView.as_view()),
 ]
